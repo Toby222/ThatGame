7237cfd12f0f35c98b717b81782757d92435c31c
Tobias Berger <toby@tobot.dev>
2023-03-29 10:50:44 +0200

    GitHub Pages setup

--- build-all.sh
+++ build-all.sh
@@ -3,7 +3,7 @@ set -x
 
 # Useful for refreshing cache after changing dependencies
 
-cargo build --target wasm32-unknown-unknown --release
-cargo build --target wasm32-unknown-unknown
-cargo build --target x86_64-unknown-linux-gnu --release
-cargo build --target x86_64-unknown-linux-gnu
+cargo build --verbose --target wasm32-unknown-unknown
+cargo build --verbose --target wasm32-unknown-unknown --release
+cargo build --verbose --target x86_64-unknown-linux-gnu
+cargo build --verbose --target x86_64-unknown-linux-gnu --release
